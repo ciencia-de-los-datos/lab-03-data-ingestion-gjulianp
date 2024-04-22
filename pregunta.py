0d635eaf273d2cdb5cd2830f2787fdd7f6589ddd
Julian <jugomezp@unal.edu.co>
2024-04-21 23:21:48 -0500

    intento 1 pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,59 @@ espacio entre palabra y palabra.
 
 
 """
+
+
+
+
+
 import pandas as pd
+
+import pandas as pd
+def load_data(filename):
+    df = pd.read_fwf(filename,
+                     skiprows=4,
+                     index_col=False,
+                     widths=[7,6,18,87],
+                     header=None,
+                     names=['cluster',
+                            'cantidad_de_palabras_clave',
+                            'porcentaje_de_palabras_clave',
+                            'principales_palabras_clave'],
+                            decimal=".",
+                            )
+    return df
+
+
+def procesamiento_datos(df):
+    
+    df["cluster"] = df["cluster"].ffill()
+    df["principales_palabras_clave"] = df.groupby(["cluster"])["principales_palabras_clave"
+    ].transform(lambda x: " ".join(x))
+    df = df.drop_duplicates(subset=["cluster"])
+    df = df.reset_index(drop=True)
+    df["cluster"] = df["cluster"].astype(int)
+   
+   
+    # Eliminar el porcentaje completamente de los valores
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace("%", "")
+    # Eliminar el porcentaje completamente de los valores
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].str.replace(",", ".")
+    # Convertir la columna 'porcentaje_de_palabras_clave' a números flotantes
+    df['porcentaje_de_palabras_clave'] = df['porcentaje_de_palabras_clave'].astype(float)
+    
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].str.strip(
+        "."
+    )
+    df["principales_palabras_clave"] = df["principales_palabras_clave"].replace(
+        r"\s+", " ", regex=True
+    )
+    return df
 
 
 def ingest_data():
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = load_data("clusters_report.txt")
+    df = procesamiento_datos(df)
     return df
+
+
+
